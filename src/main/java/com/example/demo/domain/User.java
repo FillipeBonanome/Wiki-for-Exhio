6c064bac3f094e48d6cd6d67861af736eceb8446
Fillipe <incarnialol2@gmail.com>
2025-08-08 02:11:48 -0300

    feat: user details implemented
    - implemented user details
    - fix @NotBlank for role

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,11 +1,17 @@
 package com.example.demo.domain;
 
+import com.example.demo.dto.CreateUserDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -15,7 +21,7 @@ import java.util.UUID;
 @EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -34,12 +40,50 @@ public class User {
     @NotBlank
     @Email
     private String email;
-
-    @NotBlank
+    
     @Enumerated(value = EnumType.STRING)
     private UserRole role;
 
     private Boolean active;
+
+    public User(CreateUserDTO user) {
+        this.name = user.name();
+        this.login = user.login();
+        this.password = user.password();
+        this.email = user.email();
+        this.role = UserRole.USER;
+        this.active = true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.toString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 
     //List<Comments> comments
 }
