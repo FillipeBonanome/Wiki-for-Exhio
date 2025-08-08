94a3626d1c342ebea9472b1e6ee2bc2fbb726262
Fillipe <incarnialol2@gmail.com>
2025-08-08 01:28:03 -0300

    feat: user entity
    - created user entity

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,45 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+    private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+    private Boolean active;
+    
+    //List<Comments> comments
+}
