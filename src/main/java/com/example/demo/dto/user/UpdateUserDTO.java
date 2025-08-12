d231aa35d91d64ac747ebff87c00ddf17a9fa0f8
Fillipe <incarnialol2@gmail.com>
2025-08-12 05:39:13 -0300

    chore: cleaner MethodArgumentNotValidException error message
    - MethodArgument... now have a cleaner error message

--- src/main/java/com/example/demo/dto/user/UpdateUserDTO.java
+++ src/main/java/com/example/demo/dto/user/UpdateUserDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record UpdateUserDTO(
         String name,
-        @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+        @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Your password must have an uppercase, a lowercase, a number and a symbol")
         String password
 ) {
 }
--- src/main/java/com/example/demo/infra/handler/ErrorHandler.java
+++ src/main/java/com/example/demo/infra/handler/ErrorHandler.java
@@ -6,12 +6,15 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -40,9 +43,17 @@ public class ErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
         ErrorDTO error = new ErrorDTO(
                 HttpStatus.BAD_REQUEST.value(),
-                exception.getMessage(),
+                errors.toString(),
                 request.getDescription(false),
                 LocalDateTime.now()
         );
