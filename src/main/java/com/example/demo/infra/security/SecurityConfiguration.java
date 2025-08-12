adcffdc9ad23eb3bd081883362c6066413958d09
Fillipe <incarnialol2@gmail.com>
2025-08-12 06:11:19 -0300

    fix: permissions in security config

--- src/main/java/com/example/demo/infra/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/infra/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.demo.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -27,7 +28,13 @@ public class SecurityConfiguration {
         return httpSecurity.csrf(csrf -> csrf.disable()).
                 sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
                 authorizeHttpRequests((auth) ->
-                                auth.anyRequest().permitAll())
+                                auth
+                                    .requestMatchers(HttpMethod.GET, "/monsters/**").permitAll()
+                                    .requestMatchers(HttpMethod.GET, "/hunts/**").permitAll()
+                                    .requestMatchers(HttpMethod.GET, "/quests/**").permitAll()
+                                    .requestMatchers(HttpMethod.GET, "/spells/**").permitAll()
+                                    .requestMatchers(HttpMethod.GET, "/vocations/**").permitAll()
+                                    .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
